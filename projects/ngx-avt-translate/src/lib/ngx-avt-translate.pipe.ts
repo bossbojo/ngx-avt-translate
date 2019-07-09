4d9c065bfd29adfe2d2f5f78a11d2634295e6ffa
paramat <paramat@avesta.co.th>
2019-07-09 13:40:46 +0700

    add traslate on service

--- projects/ngx-avt-translate/src/lib/ngx-avt-translate.pipe.ts
+++ projects/ngx-avt-translate/src/lib/ngx-avt-translate.pipe.ts
@@ -7,32 +7,13 @@ import { JsonLang, NgxAvtTranslateService } from './ngx-avt-translate.service';
 })
 export class NgxAvtTranslatePipe implements PipeTransform {
 
-  constructor(private json: JsonLang, private getLang: NgxAvtTranslateService, private pipeRef: ChangeDetectorRef) {
-    this.getLang.onChangeLang.subscribe(() => {
-      // this.pipeRef.detectChanges();
+  constructor(private json: JsonLang, private ser: NgxAvtTranslateService, private pipeRef: ChangeDetectorRef) {
+    this.ser.onChangeLang.subscribe(() => {
       this.pipeRef.markForCheck();
     });
   }
   transform(value: string, args?: any) {
-    const key = value.split('.');
-    if (key.length === 0) {
-      return value;
-    }
-    let buff = this.json.jsonOject[this.getLang.getDefualtLang()];
-    if (!buff) {
-      return value;
-    }
-    key.forEach(k => {
-      if (buff[k]) {
-        buff = buff[k];
-      } else {
-        return value;
-      }
-    });
-    if ((typeof buff === 'object' || typeof buff === 'function') && (buff !== null)) {
-      return value;
-    }
-    return buff;
+    return this.ser.onTranslate(value, this.json);
   }
 
 }
--- projects/ngx-avt-translate/src/lib/ngx-avt-translate.service.ts
+++ projects/ngx-avt-translate/src/lib/ngx-avt-translate.service.ts
@@ -16,11 +16,33 @@ export interface SupportLang {
 export class NgxAvtTranslateService {
   defualtLang = 'en';
   onChangeLang = new Subject();
+  constructor() { }
   setDefualtLang(key: string) {
     this.defualtLang = key;
     this.onChangeLang.next(key);
   }
   getDefualtLang(): string {
     return this.defualtLang;
+  }
+  onTranslate(value, json) {
+    const key = value.split('.');
+    if (key.length === 0) {
+      return value;
+    }
+    let buff = json.jsonOject[this.getDefualtLang()];
+    if (!buff) {
+      return value;
+    }
+    key.forEach(k => {
+      if (buff[k]) {
+        buff = buff[k];
+      } else {
+        return value;
+      }
+    });
+    if ((typeof buff === 'object' || typeof buff === 'function') && (buff !== null)) {
+      return value;
+    }
+    return buff;
   }
 }
